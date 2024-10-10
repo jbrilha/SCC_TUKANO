2d6e9d89cd77c1f4e8e01eb879f57614093ed569
jbrilha <j.brilha@campus.fct.unl.pt>
2024-10-10 15:32:04 +0100

    feat: Redis caching WIP

--- src/main/java/tukano/impl/cache/RedisCache.java
+++ src/main/java/tukano/impl/cache/RedisCache.java
@@ -1,9 +1,14 @@
 package tukano.impl.cache;
 
+import java.util.function.Supplier;
+
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+import tukano.api.Result;
+import tukano.api.Result.ErrorCode;
+import utils.JSON;
 
-public class RedisCache {
+public class RedisCache /* implements Cache */ {
     private static final String RedisHostname = System.getenv("REDIS_HOST");
     private static final String RedisKey = System.getenv("REDIS_PK");
     private static final int REDIS_PORT = 6380;
@@ -28,4 +33,58 @@ public class RedisCache {
         instance = new JedisPool(poolConfig, RedisHostname, REDIS_PORT, REDIS_TIMEOUT, RedisKey, Redis_USE_TLS);
         return instance;
     }
+
+    // @Override
+    public static <T> Result<T> getOne(String key, Class<T> clazz) {
+        try (var jedis = RedisCache.getCachePool().getResource()) {
+            var obj = JSON.decode(jedis.get(key), clazz);
+
+            if(obj.equals("nil")) { // Golang mentioned??
+                return Result.error(ErrorCode.NOT_FOUND);
+            }
+
+            return Result.ok(obj);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.error(ErrorCode.INTERNAL_ERROR);
+        }
+    }
+
+    // @Override
+    public static <T> Result<T> insertOne(String key, T obj) {
+        try (var jedis = RedisCache.getCachePool().getResource()) {
+            jedis.set(key, JSON.encode(obj));
+            jedis.expire(key, 1800);
+            // TODO set expiration based on obj type
+            // TODO add to list, i.e most_recent_obj ...
+
+            return Result.ok(obj);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.error(ErrorCode.INTERNAL_ERROR);
+        }
+    }
+
+    // @Override
+    public static <T> Result<T> invalidate(String... keys) {
+        try (var jedis = RedisCache.getCachePool().getResource()) {
+            jedis.del(keys);
+            // TODO remove from list, i.e most_recent_obj ...
+
+            return Result.ok();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.error(ErrorCode.INTERNAL_ERROR);
+        }
+    }
+
+	<T> Result<T> tryCatch( Supplier<T> supplierFunc) {
+		try {
+			return Result.ok(supplierFunc.get());			
+		} catch( Exception x ) {
+			x.printStackTrace();
+			return Result.error( ErrorCode.INTERNAL_ERROR);						
+		}
+	}
+
 }
--- src/main/java/utils/DB.java
+++ src/main/java/utils/DB.java
@@ -8,7 +8,10 @@ import org.hibernate.Session;
 
 import tukano.api.Result;
 import tukano.api.User;
+import tukano.api.Short;
+import tukano.api.Result.ErrorCode;
 import tukano.impl.storage.azure.CosmosDB;
+import tukano.impl.cache.RedisCache;
 
 public class DB {
 
@@ -24,23 +27,57 @@ public class DB {
 	
 	public static <T> Result<T> getOne(String id, Class<T> clazz) {
         if(clazz.equals(User.class)) {
-		    return CosmosDB.getInstance().getOne(id, clazz);
+            var key = "user:" + id;
+            var res = RedisCache.getOne(key, clazz);
+
+            if(!res.isOK()) {
+		        return CosmosDB.getInstance().getOne(id, clazz);
+            } else {
+                System.out.println("Cached result: " + res);
+                return res;
+            }
+                
         }
+
 		return Hibernate.getInstance().getOne(id, clazz);
 	}
 	
 	public static <T> Result<T> deleteOne(T obj) {
+        String key = null;
+
+        if(obj instanceof User user) {
+            key = "user:" + user.getUserId();
+        } else if(obj instanceof Short shrt) {
+            key = "short:" + shrt.getShortId();
+        }
+        if(key != null)
+            RedisCache.invalidate(key);
+
 		return Hibernate.getInstance().deleteOne(obj);
 	}
 	
 	public static <T> Result<T> updateOne(T obj) {
+        String key = null;
+
+        if(obj instanceof User user) {
+            key = "user:" + user.getUserId();
+        } else if(obj instanceof Short shrt) {
+            key = "short:" + shrt.getShortId();
+        }
+        if(key != null)
+            RedisCache.invalidate(key);
+
 		return Hibernate.getInstance().updateOne(obj);
 	}
 	
 	public static <T> Result<T> insertOne( T obj) {
 		System.err.println("DB.insert:" + obj );
 
-        if(obj instanceof User) {
+        if(obj instanceof User user) {
+            var key = "user:" + user.getUserId();
+
+            RedisCache.insertOne(key, obj);
+
 		    return Result.errorOrValue(CosmosDB.getInstance().insertOne(obj), obj);
         }
 		return Result.errorOrValue(Hibernate.getInstance().persistOne(obj), obj);
