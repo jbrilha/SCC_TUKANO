7933184edecc62d23eed33477a27d9c0aa473f27
jbrilha <j.brilha@campus.fct.unl.pt>
2024-10-31 14:35:06 +0000

    chore: Misc comments / Cleanup

--- src/main/java/tukano/impl/Token.java
+++ src/main/java/tukano/impl/Token.java
@@ -9,8 +9,10 @@ public class Token {
 
 	private static final String DELIMITER = "-";
 	private static final long MAX_TOKEN_AGE = 10000;
-	private static String secret;
+	private static String secret = System.getProperty("SECRET");
 
+    // TODO this was called in the previous tukanoRestServer (before deployment)
+    // so instead I set the secret here, maybe not ideal?
 	public static void setSecret(String s) {
 		secret = s;
 	}
